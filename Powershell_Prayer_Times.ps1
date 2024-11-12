a0faa0be5b2de06f1c55ece8b5bfba287fffe08d
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2024-11-12 01:46:55 +0100

    add changes

--- Powershell_Prayer_Times.ps1
+++ Powershell_Prayer_Times.ps1
@@ -1,25 +1,21 @@
-Write-Host " ####   ##   ##    ####   ######      ####   ##  ##  "
-Write-Host " ##  ##  ### ###   ##  ##   ##  ##      ##    ##  ##  "
-Write-Host " ##  ##  #######       ##   ##  ##      ##    ##  ##  "
-Write-Host "  #####  ## # ##      ##    ##  ##      ##    ##  ##  "
-Write-Host "     ##  ##   ##     ##     #####       ##    ##  ##  "
-Write-Host "    ##   ##   ##    ##      ##      ##  ##    ##  ##  "
-Write-Host "  ###    ##   ##   ######  ###       ####      ####   "
-Write-Host "                                                      "
-Write-Host "9M2PJU Powershell Prayer Times v1.0"
+Write-Host "Powershell Prayer Times v1.0"
 Write-Host ""
 
 function Get-UserLocation {
     $url = "http://ip-api.com/json/"
     $location = Invoke-RestMethod -Uri $url
     if ($location) {
-        return @{ latitude = $location.lat; longitude = $location.lon }
+        return @{
+	    latitude = $location.lat;
+	    longitude = $location.lon;
+	    country = $location.country;
+	    city = $location.city;
+	}
     } else {
-        Write-Error "Could not retrieve location data."
+	Write-Error "Could not retrieve location data."
         exit
     }
 }
-
 
 function Get-PrayerTimes {
     param (
@@ -32,8 +28,7 @@ function Get-PrayerTimes {
         "Isha" = 18
     }
 
-    $apiUrl = "https://api.aladhan.com/v1/timings?latitude=$latitude&longitude=$longitude&method=17"
-
+    $apiUrl = "https://api.aladhan.com/v1/timings?latitude=$latitude&longitude=$longitude&method=3"
     $response = Invoke-RestMethod -Uri $apiUrl -Method Get
 
     if ($response.code -eq 200) {
@@ -45,24 +40,24 @@ function Get-PrayerTimes {
     }
 }
 
-
 $location = Get-UserLocation
 $latitude = $location.latitude
 $longitude = $location.longitude
-
+$country = $location.country
+$city = $location.city
 
 $prayerTimes = Get-PrayerTimes -latitude $latitude -longitude $longitude
+$currentDate = Get-Date -Format "dddd, dd MMMM, yyyy"
 
-
-$currentDate = Get-Date -Format "dddd, MMMM dd, yyyy"
-
-
-Write-Host "Prayer Times for ${currentDate}:"
+Write-Host "Prayer Times for ${city}, ${country}, ${currentDate}:"
 Write-Host "Fajr: $($prayerTimes.Fajr)"
+Write-Host "Sunrise: $($prayerTimes.Sunrise)"
 Write-Host "Dhuhr: $($prayerTimes.Dhuhr)"
 Write-Host "Asr: $($prayerTimes.Asr)"
 Write-Host "Maghrib: $($prayerTimes.Maghrib)"
 Write-Host "Isha: $($prayerTimes.Isha)"
-
+Write-Host "Midnight: $($prayerTimes.Midnight)"
+Write-Host "Qiyam: $($prayerTimes.Lastthird)"
 
 Read-Host -Prompt "Press Enter to exit"
+
